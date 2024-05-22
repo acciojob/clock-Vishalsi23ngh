3f5a91c1df37d4f38510735d424197722ade1d0b
Vishal <100362137+Vishalsi23ngh@users.noreply.github.com>
2024-05-22 21:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const timerElement = document.getElementById("timer");
+function updateTime(){
+	const now = new Date();
+	// extract year
+	const year = now.getFullYear();
+	const month = String(now.getMonth()+1).padStart(2,"0");
+
+	const day = String(now.getDate()).padStart(2,"0");
+	const hours = now.getHours();
+	const minutes = String(now.getMinutes()).padStart(2,"0");
+	const seconds = String(now.getSecond()).padStart(2,"0");
+
+	const ampm = hours >= 12 ?"PM" :"AM";
+	const formattedHours = hours % 12 || 12;
+	const formattedTime =`${month}/${day}/${year} ${formattedHours} : ${minutes} :${seconds} ${ampm}`
+
+	
+	
+	console.log({formattedTime});
+	timerElement.textContent = formattedTime;
+}
+setInterval(updateTime,1000);
